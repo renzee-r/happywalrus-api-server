75ab78eaaa91e1b0fff6ac775c7b31ba564d9b9b
renzee-r <rj4219@gmail.com>
2019-11-25 09:41:36 -1000

    Updated gunicorn config

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
 bind = ":" + str(PORT)
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2 * multiprocessing.cpu_count()
+timeout = 120
+
+errorlog = '-'
+loglevel = 'debug'
+accesslog = '-'
+
+spew = True
